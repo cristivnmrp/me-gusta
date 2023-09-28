e1e17cacb18f2f3f9b6b8a06dbee52b2debf4bf4
cristivnmrp <c.morales.p95@gmail.com>
2023-09-28 11:18:56 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+function sumar1(elemento){
+    let elementoMeGustas = document.querySelector('#numeroMeGusta1');
+    let numeroMeGustas = elementoMeGustas.innerText;
+    numeroMeGustas ++;
+    elementoMeGustas.innerText = numeroMeGustas;
+    }
+    function sumar2(elemento){
+        let elementoMeGustas = document.querySelector('#numeroMeGusta2');
+        let numeroMeGustas = elementoMeGustas.innerText;
+        numeroMeGustas ++;
+        elementoMeGustas.innerText = numeroMeGustas;
+    }
+    
+    function sumar3(elemento){
+    let elementoMeGustas = document.querySelector('#numeroMeGusta3');
+    let numeroMeGustas = elementoMeGustas.innerText;
+    numeroMeGustas ++;
+    elementoMeGustas.innerText = numeroMeGustas;
+    }
